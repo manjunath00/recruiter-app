3631ef303c5dc19889c04cecc3e1ffbedde2d7ab
manjunath00 <kadrimanjunath56@gmail.com>
2020-04-24 22:39:06 +0530

    initial setup & common files

--- src/App.js
+++ src/App.js
@@ -1,25 +1,45 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { Route, Router, Switch } from "react-router-dom";
+import { connect } from "react-redux";
+
+import history from "./history";
+import Header from "./components/Header";
+import NavigationBar from "./components/NavigationBar";
+import SideBar from "./components/SideBar";
+import Main from "./components/Main";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router history={history}>
+      <div className="container">
+        <Header />
+        <NavigationBar />
+        <Switch>
+          <Route exact={true} to="/" component={Main} />
+          <Route
+            exact={true}
+            to="/candidates"
+            render={(props) => <Main {...props} type="liveJobs" />}
+          />
+          <Route
+            exact={true}
+            to="/jobs/live"
+            render={(props) => <Main {...props} type="liveJobs" />}
+          />
+          <Route
+            exact={true}
+            to="/jobs/accepted"
+            render={(props) => <Main {...props} type="acceptedJobs" />}
+          />
+          <Route
+            exact={true}
+            to="/jobs/rejected"
+            render={(props) => <Main {...props} type="rejectedJobs" />}
+          />
+        </Switch>
+        <SideBar />
+      </div>
+    </Router>
   );
 }
 
